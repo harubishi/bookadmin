a3ae4d2a8f4d9ad36c1cacf753fec4018a1eb721
harubishi <harubishi.91940@gmail.com>
2017-01-22 14:41:58 +0900

    edit_config/database.php

--- Config/database.php
+++ Config/database.php
@@ -2,12 +2,18 @@
 
 class Database
 {
+
+	private $pdo;
+
+
 	//データベースの接続をする関数
 	public function setConnection()
 	{
 		$pdo =new PDO ("mysql:host=localhost;dbname=test","root","root");
 		$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 		$pdo->exec("SET NAMES utf8");
+
+		$this->pdo = $pdo;
 
 	}
 }
